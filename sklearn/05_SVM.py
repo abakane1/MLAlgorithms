ede0ed79ba0acf175bc9f5f40ae19cae1ac1a15c
abakane1 <mrzzl1987@gmail.com>
2018-07-18 14:25:10 -0400

    SVMs

---
+++ sklearn/05_SVM.py
@@ -0,0 +1,79 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.svm import LinearSVC, SVC
+from sklearn.datasets import make_moons
+
+import matplotlib.pyplot as plt
+
+iris = datasets.load_iris()
+X = iris["data"][:, (2, 3)]
+y = (iris["target"] == 2).astype(np.float64)
+
+
+def make_meshgrid(x, y, h=.02):
+    """Create a mesh of points to plot in
+
+    Parameters
+    ----------
+    x: data to base x-axis meshgrid on
+    y: data to base y-axis meshgrid on
+    h: stepsize for meshgrid, optional
+
+    Returns
+    -------
+    xx, yy : ndarray
+    """
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    return xx, yy
+
+def Draw(model):
+    X0, X1 = X[:, 0], X[:, 1]
+    xx, yy = make_meshgrid(X0, X1)
+    ax = plt.figure().add_subplot(111)
+    Z = model.predict(np.c_[xx.ravel(),yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    ax.contourf(xx,yy, Z)
+    ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+    ax.set_xlim(xmin=xx.min(), xmax=xx.max())
+    ax.set_ylim(ymin=yy.min(), ymax=yy.max())
+    ax.set_xlabel('Sepal length')
+    ax.set_ylabel('Sepal width')
+    ax.set_xticks(())
+    ax.set_yticks(())
+    plt.show()
+
+def svm_clf():
+    svm_clf = Pipeline((
+        ("scaler", StandardScaler()),
+        ("linear_svc", LinearSVC(C=1, loss="hinge")),
+    ))
+
+    svm_clf.fit(X, y)
+    Draw(svm_clf)
+#svm_clf()
+
+def Polynomial_svm_clf():
+    Polynomial_svm_clf = Pipeline((
+        ("poly_features", PolynomialFeatures(degree=3)),
+        ("scaler", StandardScaler()),
+        ("svm_clf", LinearSVC(C=10, loss="hinge"))
+    ))
+
+    Polynomial_svm_clf.fit(X, y)
+    Draw(Polynomial_svm_clf)
+#Polynomial_svm_clf()
+
+def Poly_kernel_svm_clf ():
+    poly_kernel_svm_clf = Pipeline((
+        ("scaler", StandardScaler()),
+        ("svm_clf", SVC(kernel="poly", degree=3, coef0=1, C=5))
+    ))
+    poly_kernel_svm_clf.fit(X, y)
+    Draw(poly_kernel_svm_clf)
+
+Poly_kernel_svm_clf()
