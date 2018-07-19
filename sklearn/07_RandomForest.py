ae2c117aec085315e0ee1185e0644bff7646c468
abakane1 <mrzzl1987@gmail.com>
2018-07-18 23:28:40 -0400

    Random Forest

--- sklearn/07_RandomForest.py
+++ sklearn/07_RandomForest.py
@@ -1,6 +1,17 @@
 from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
+X, y = make_moons(noise=0.3, random_state=0)
+X = StandardScaler().fit_transform(X)
+X_train, X_test, y_tain, y_test = train_test_split(X, y, test_size=.4, random_state=42)
+#plt.scatter(X[:,0],X[:,1])
+#plt.show()
 
 log_clf = LogisticRegression()
 rnd_clf = RandomForestClassifier()
@@ -10,3 +21,13 @@ voting_clf = VotingClassifier(
     estimators=[('lr', log_clf), ('rf', rnd_clf), ('svc', svm_clf)],
     voting='hard'
 )
+
+
+def Clf_accuracy():
+    for clf in (log_clf, rnd_clf, svm_clf, voting_clf):
+        clf.fit(X_train, y_tain)
+        y_pred = clf.predict(X_test)
+        print(clf.__class__.__name__, accuracy_score(y_test, y_pred))
+
+
+Clf_accuracy()
